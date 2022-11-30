21c516702418fc102fbc50cc65318a22386efb7a
tyler <moroztyler@gmail.com>
2022-11-30 12:41:13 -0700

    Add base HTML with form elements, add css to hide form, add javascript to manipulate form data to create book objects.

--- script.js
+++ script.js
@@ -1,123 +1,52 @@
-const bookShelf = document.querySelector('.books-tab');
-const addBookButton =  document.getElementById('bookButton');
-const buttonTab = document.querySelector('.button-tab');
-const formElement = document.getElementById('formElement');
-let addBook;
-let addAuthor;
-let deleteButton;
-let readButton;
-let bookIndex;
-let selectedValue;
-
 let myLibrary = [];
 let libraryQueue = [];
+const body = document.getElementById("body");
+const formDiv = document.getElementById("form-div");
+const bookForm = document.getElementById("book-form");
+const newBookBtn = document.getElementById("new-book-button");
+const addBookBtn = document.getElementById("add-book");
 
-class Book {
-    constructor( title, author, pageCount ) {
-        this.title = title;
-        this.author = author;
-        this.pageCount = pageCount;
-    }
-
-    readYet() {
-        const radioBtns = document.querySelectorAll('input[name="read"]');
-        
-        for(const radio of radioBtns){
-            if(radio.checked){
-                selectedValue = radio.value;
-                break;
-            }
-        }
-    }
-
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
+function displayLibrary() {
+    for(let i = 0; i < libraryQueue.length; i++){
+        const addBook = document.createElement("div");
+        const addTitle = document.createElement("p");
+        const addAuthor = document.createElement("p");
+        addBook.style.border = "1px solid black";
+        addBook.style.width = "200px";
+        addTitle.textContent = libraryQueue[i].title;
+        addAuthor.textContent = libraryQueue[i].author;
+        body.appendChild(addBook);
+        addBook.appendChild(addTitle);
+        addBook.appendChild(addAuthor);
+    }
+}
 
-addBookButton.addEventListener('click', function() {
-    let book = new Book();
-    book.title = document.getElementById("title").value;
-    book.author = document.getElementById("author").value;
-    book.pageCount = document.getElementById("pages").value;
-    book.readYet();
-    book.readStatus = selectedValue;
+function displayForm() {
+    formDiv.style.display = "block";
+}
+
+newBookBtn.addEventListener('click', displayForm);
+
+function displayBook(event) {
+    event.preventDefault()
+    const book = new Book();
+    book.title = document.getElementById("title").value; 
+    book.author = document.getElementById("author").value; 
+    book.pages = document.getElementById("pages").value; 
+    book.read = document.getElementById("read").value; 
     myLibrary.push(book);
-    book['bookIndex'] = myLibrary.indexOf(book);
     libraryQueue.push(book);
-    displayBooks(book);
-    storeBook();
-    formElement.reset();
+    bookForm.reset();
+    displayLibrary();
     libraryQueue.length = 0;
-})
-
-
-function displayBooks(book){
-    let addBook = document.createElement('div');
-    let addAuthor = document.createElement('p');
-    let deleteButton = document.createElement('button');
-    let readButton = document.createElement('button');
-
-    addBook.classList.add('book-div');
-    
-    deleteButton.textContent = `Delete`;
-    readButton.textContent = selectedValue;
-    readButton.dataset.status = selectedValue;
-    
-    
-    for(let i = 0; i < libraryQueue.length; i++){ 
-        addBook.textContent = `"${libraryQueue[i]['title']}"`;
-        addAuthor.textContent = `-${libraryQueue[i]['author']}`;
-                
-    }
-
-    bookShelf.appendChild(addBook);
-    addBook.appendChild(addAuthor);
-    addBook.appendChild(deleteButton);
-    addBook.appendChild(readButton);
-
-
-    deleteButton.addEventListener('click', function(){ 
-        addBook.remove();
-        myLibrary.splice(book['bookIndex'], 1);
-        for(const book of myLibrary){
-            book['bookIndex'] = myLibrary.indexOf(book);
-        }
-        localStorage.removeItem("Books");
-        storeBook();
-    })
-
-    readButton.addEventListener('click', function(){
-        if(readButton.dataset.status === "Read"){
-            readButton.dataset.status = "Not Read";
-            readButton.textContent = "Not Read";
-        } else if (readButton.dataset.status === "Not Read"){
-            readButton.dataset.status = "Read";
-            readButton.textContent = "Read";
-        }
-        book.readStatus = readButton.dataset.status;
-        localStorage.removeItem('Books');
-        storeBook();
-    })
+    console.log(myLibrary);
 }
 
-window.addEventListener('load', function(){
-    let retrieveLibrary = localStorage.getItem("Books");
-    let storedBooks = JSON.parse(retrieveLibrary);
-    for(let i = 0; i < storedBooks.length; i++){
-        let book = new Book();
-        book.title = storedBooks[i].title
-        book.author = storedBooks[i].author
-        book.pageCount = storedBooks[i].pageCount;
-        selectedValue = storedBooks[i].readStatus;
-        book.readStatus = selectedValue;
-        myLibrary.push(book);
-        book['bookIndex'] = myLibrary.indexOf(book);
-        libraryQueue.push(book);
-        displayBooks(book);
-        libraryQueue.length = 0;
-    }
-})
-
-function storeBook(){
-    localStorage.setItem("Books", JSON.stringify(myLibrary));
-}
-
+addBookBtn.addEventListener('click', displayBook);
