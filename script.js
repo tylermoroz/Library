b1bb5986b496d4099a95637cbc4ef17632857f08
tylermoroz <moroztyler@gmail.com>
2023-10-20 14:53:12 -0600

    Add required attribute and min attribute to input elements.

--- script.js
+++ script.js
@@ -9,96 +9,95 @@ const body = document.getElementById("body");
 let bookIndex;
 
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function displayLibrary(book) {
-    let addBook = document.createElement("div");
-    let addTitle = document.createElement("p");
-    let addAuthor = document.createElement("p");
-    let deleteBtn = document.createElement("button");
-    let readStatusBtn = document.createElement("button");
+  let addBook = document.createElement("div");
+  let addTitle = document.createElement("p");
+  let addAuthor = document.createElement("p");
+  let deleteBtn = document.createElement("button");
+  let readStatusBtn = document.createElement("button");
 
-    for(let i = 0; i < libraryQueue.length; i++){ 
-        addBook.classList.add("book-class");
-        addTitle.textContent = libraryQueue[i].title;
-        addAuthor.textContent = libraryQueue[i].author;
-        deleteBtn.textContent = "DELETE";
-        deleteBtn.classList.add("button-class");
-        readStatusBtn.classList.add("button-class");
-        addTitle.classList.add("title-class");
-        addAuthor.classList.add("author-class");
+  for (let i = 0; i < libraryQueue.length; i++) {
+    addBook.classList.add("book-class");
+    addTitle.textContent = libraryQueue[i].title;
+    addAuthor.textContent = libraryQueue[i].author;
+    deleteBtn.textContent = "DELETE";
+    deleteBtn.classList.add("button-class");
+    readStatusBtn.classList.add("button-class");
+    addTitle.classList.add("title-class");
+    addAuthor.classList.add("author-class");
 
-        if(book.read == true) {
-            book.read = true;
-            readStatusBtn.textContent = "Read";
-            readStatusBtn.style.backgroundColor = "rgb(171, 252, 151)";
-        } else {
-            book.read = false;
-            readStatusBtn.textContent = " Not Read";
-            readStatusBtn.style.backgroundColor = "rgb(255, 120, 120)";
-        }
+    if (book.read == true) {
+      book.read = true;
+      readStatusBtn.textContent = "Read";
+      readStatusBtn.style.backgroundColor = "rgb(171, 252, 151)";
+    } else {
+      book.read = false;
+      readStatusBtn.textContent = "Not Read";
+      readStatusBtn.style.backgroundColor = "rgb(255, 120, 120)";
     }
+  }
 
-    libraryDiv.appendChild(addBook);
-    addBook.appendChild(addTitle);
-    addBook.appendChild(addAuthor);
-    addBook.appendChild(deleteBtn);
-    addBook.appendChild(readStatusBtn);
+  libraryDiv.appendChild(addBook);
+  addBook.appendChild(addTitle);
+  addBook.appendChild(addAuthor);
+  addBook.appendChild(deleteBtn);
+  addBook.appendChild(readStatusBtn);
 
-    deleteBtn.addEventListener('click', function() {
-        addBook.remove();
-        myLibrary.splice(book['bookIndex'], 1);
-        for(const book of myLibrary){
-            book['bookIndex'] = myLibrary.indexOf(book);
-        }
-    })
+  deleteBtn.addEventListener("click", function () {
+    addBook.remove();
+    myLibrary.splice(book["bookIndex"], 1);
+    for (const book of myLibrary) {
+      book["bookIndex"] = myLibrary.indexOf(book);
+    }
+  });
 
-    readStatusBtn.addEventListener('click', function() {
-        if(book.read == false) {
-            book.read = true;
-            readStatusBtn.textContent = "Read";
-            readStatusBtn.style.backgroundColor = "rgb(171, 252, 151)";
-        } else {
-            book.read = false;
-            readStatusBtn.textContent = " Not Read";
-            readStatusBtn.style.backgroundColor = "rgb(255, 120, 120)";
-        }
-    });
-
+  readStatusBtn.addEventListener("click", function () {
+    if (book.read == false) {
+      book.read = true;
+      readStatusBtn.textContent = "Read";
+      readStatusBtn.style.backgroundColor = "rgb(171, 252, 151)";
+    } else {
+      book.read = false;
+      readStatusBtn.textContent = " Not Read";
+      readStatusBtn.style.backgroundColor = "rgb(255, 120, 120)";
+    }
+  });
 }
 
 function displayForm() {
-    formDiv.style.display = "block";
-    if(newBookBtn.textContent == "NEW BOOK"){
-        newBookBtn.textContent = "CLOSE";
-    } else if (newBookBtn.textContent == "CLOSE") {
-        newBookBtn.textContent = "NEW BOOK";
-        formDiv.style.display = "none";
-    }
+  formDiv.style.display = "block";
+  if (newBookBtn.textContent == "NEW BOOK") {
+    newBookBtn.textContent = "CLOSE";
+  } else if (newBookBtn.textContent == "CLOSE") {
+    newBookBtn.textContent = "NEW BOOK";
+    formDiv.style.display = "none";
+  }
 }
 
-newBookBtn.addEventListener('click', displayForm);
+newBookBtn.addEventListener("click", displayForm);
 newBookBtn.classList.add("button-class");
 
 function displayBook(event) {
-    event.preventDefault()
-    let book = new Book();
-    book.title = document.getElementById("title").value; 
-    book.author = document.getElementById("author").value; 
-    book.pages = document.getElementById("pages").value; 
-    book.read = document.getElementById("read").checked; 
-    myLibrary.push(book);
-    book['bookIndex'] = myLibrary.indexOf(book);
-    libraryQueue.push(book);
-    bookForm.reset();
-    displayLibrary(book);
-    libraryQueue.length = 0;
-    console.log(myLibrary);
+  event.preventDefault();
+  let book = new Book();
+  book.title = document.getElementById("title").value;
+  book.author = document.getElementById("author").value;
+  book.pages = document.getElementById("pages").value;
+  book.read = document.getElementById("read").checked;
+  myLibrary.push(book);
+  book["bookIndex"] = myLibrary.indexOf(book);
+  libraryQueue.push(book);
+  bookForm.reset();
+  displayLibrary(book);
+  libraryQueue.length = 0;
+  console.log(myLibrary);
 }
 
-addBookBtn.addEventListener('click', displayBook);
+addBookBtn.addEventListener("click", displayBook);
 addBookBtn.classList.add("button-class");
